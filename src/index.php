6b75d8b22fcd003f41e24e3361e83d8c3c5f4cf7
Tanakamonzaemon <2201225@s.asojuku.ac.jp>
2024-01-10 14:09:30 +0900

    a

--- src/index.php
+++ src/index.php
@@ -6,23 +6,9 @@
 </head>
 <body>
     <h1>今日の日付を選んでください</h1>
-    5月22日にすると、、！？
+   
     <form action="kadai2-1-output.php" method="post">
-        <select name="month">
-            <?php
-            for ($i = 1; $i <= 12; $i++) {
-                echo '<option value="' . $i . '">' . $i . '月</option>';
-            }
-            ?>
-        </select>
-        <select name="day">
-            <?php
-            for ($i = 1; $i <= 31; $i++) {
-                echo '<option value="' . $i . '">' . $i . '日</option>';
-            }
-            ?>
-        </select>
-        <button type="submit">送信</button>
+        <button type="submit" name="insert">登録</button>
     </form>
 </body>
 </html>
